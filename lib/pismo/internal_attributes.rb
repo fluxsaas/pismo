598dbeb71c0bd3878359c2a3ae88dc9488c1adf0
kalle saas <kalle@easypep.de>
2013-04-07 02:55:52 +0300

    added "FAVICON_MATCHES = " back. stupid github edit :(

--- lib/pismo/internal_attributes.rb
+++ lib/pismo/internal_attributes.rb
@@ -33,7 +33,7 @@ module Pismo
     ]
 
     
-    = [
+    FAVICON_MATCHES = [
       ['link[@rel="fluid-icon"]', lambda { |el| el.attr('href') }],      # Get a Fluid icon if possible..
       ['link[@rel="shortcut icon"]', lambda { |el| el.attr('href') }],
       ['link[@rel="Shortcut Icon"]', lambda { |el| el.attr('href') }],
